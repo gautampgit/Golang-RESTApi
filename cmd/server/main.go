f36ecb58f2543239c4f354e17f4183b37d22b250
gautampgit <gautamk30@gmail.com>
2021-10-25 13:19:50 +0530

    Structing the application

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
+
+//App - struct to declare pointer
+// to database connections and messageing queues
+type App struct{}
+
+//Run functions sets up the application
+func (a *App) Run() error {
+	fmt.Println("Setting up our App")
+	return nil
+}
 
 func main() {
 	fmt.Println("API v1.0")
+
+	if err := App.Run(); err != nil {
+		log.Println("Uanble to start the REST API", err)
+	}
 }
